7db6a6a970b864e95bc4d27012a56b46d0d178dc
Gemma Standley <51327651+gstandley21@users.noreply.github.com>
2019-07-06 14:02:42 -0400

    Add sketch.js and index.html files

---
+++ sketch.js
@@ -0,0 +1,44 @@
+let xspacing = 16; // Distance between each horizontal location
+let w; // Width of entire wave
+let theta = 0.0; // Start angle at 0
+let amplitude = 75.0; // Height of wave
+let period = 500.0; // How many pixels before the wave repeats
+let dx; // Value for incrementing x
+let yvalues; // Using an array to store height values for the wave
+let fontsize = 12;
+let words = ["it", "comes", "and", "goes", "in", "waves"];
+
+function setup() {
+  createCanvas(710, 400);
+  w = width + 16;
+  dx = (TWO_PI / period) * xspacing;
+  yvalues = new Array(floor(w / xspacing));
+  textSize(fontsize);
+}
+
+function draw() {
+  background(0);
+  calcWave();
+  renderWave();
+  textAlign(LEFT);
+}
+
+function calcWave() {
+  theta += 0.02;
+
+  let x = theta;
+  for (let i = 0; i < yvalues.length; i++) {
+    yvalues[i] = sin(x) * amplitude;
+    x += dx;
+  }
+}
+
+function renderWave() {
+  noStroke();
+  fill('#66b2ff');
+  for (let x = 0; x < yvalues.length; x++) {
+      text(words[x%6],x * xspacing, height / 2 + yvalues[x]);
+    }
+}
+
+
